dd524145f9fa727dfa3385c76a85a237ab28d706
Flynn <flynnigan.tan@gmail.com>
2017-07-12 19:33:40 -0500

    updated serverjs

---
+++ server.js
@@ -0,0 +1,58 @@
+/*
+server.js
+- require express, body-parser, path
+- reservation array
+
+DONE - index.html
+- css - jumbotron
+- show waitlist object
+- show table link
+- view tables link
+- make reservation link
+
+tables.html
+- css - jumbotron
+- two panels - tables & wait list
+
+DONE - reservations.html
+- css - jumbotron
+- form
+- handles reservations, pass to server
+
+DONE - github repo
+heroku deployment
+*/
+
+// Dependencies
+// =============================================================
+
+var express = require("express");
+var bodyparser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Reservations Data
+// =============================================================
+
+var tables = [];
+var waitlist = [];
+
+// Routing
+// =============================================================
+
+// Start listening
+// =============================================================
+app.listen(PORT, function(){
+	console.log("App listening on PORT " + PORT + ".");
+});
